31e26a6026027df8bc48f421f87c1e8e7b22ab25
af40985 <gobinath.b2@anthem.com>
2018-12-17 21:45:29 +0530

    Database path changed to run in docker

--- express-server/database/db.js
+++ express-server/database/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var db = mongoose.connect('mongodb://127.0.0.1:27017/ProjectManagerApp', { userNewUrlParser: true }, function(err, response) {
+var db = mongoose.connect('mongodb://database/ProjectManagerApp', { userNewUrlParser: true }, function(err, response) {
     if (!!err) {
         console.log('Currently we are not able to connect it: ' + err );
     } else {
